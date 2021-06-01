9afb2f9669961e2893018337b711442bb8b2a306
らいか <raika4789@gmail.com>
2021-06-02 01:25:52 +0900

    静的なCRUDの実装

--- index.js
+++ index.js
@@ -3,21 +3,50 @@ var app = exp();
 
 var mysql = require("mysql");
 var connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "KinRai08011!",
-    database: "todoapp"
+  host: "localhost",
+  user: "root",
+  password: "KinRai08011!",
+  database: "todoapp",
 });
-
 
 app.get("/", function (req, res) {
-  connection.query('select * from todo', function (error, results, fields) {
-      if (error) throw error;
-      res.send(results);
-  });
+  res.send("取得したいデータのURLにアクセスしてください");
 });
 
-//任意のポート番号
+app.get("/todos", function (req, res) {
+  connection.query("select * from todo", function (error, results, fields) {
+    if (error) throw error;
+    res.send(results);
+  });
+});
+app.post("/todos", function (req, res) {
+  connection.query(
+    'insert into todo(user_id,title,description)values(1,"test","this is test todo")',
+    function (error, results, fields) {
+      if (error) throw error;
+      res.send(results);
+    }
+  );
+});
+app.put("/todos", function (req, res) {
+  connection.query(
+    'update todo set title = "testa" where id = 2',
+    function (error, results, fields) {
+      if (error) throw error;
+      res.send(results);
+    }
+  );
+});
+app.delete("/todos", function (req, res) {
+  connection.query(
+    "delete from todo where id = 1",
+    function (error, results, fields) {
+      if (error) throw error;
+      res.send(results);
+    }
+  );
+});
+
 app.listen(3000, function () {
   console.log("成功");
-})
+});
